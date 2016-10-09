2c8494927bd26bdfdbfad059a566bd9d9bd4dee8
Eric Herman <eric@freesa.org>
2016-10-09 13:47:56 +0200

    LGPLv3+
    added copyright boilerplate to *.c and *.h files
    renamed files to be more traditional and make it easier
    for tools to auto-detect LGPLv3 license

--- ts_options.h
+++ ts_options.h
@@ -1,5 +1,24 @@
 #ifndef TS_OPTIONS_H
 #define TS_OPTIONS_H
+/*
+   ts_options.h: options structure for test sorts
+   Copyright (C) 2015, 2016 Eric Herman <eric@freesa.org>
+
+   This work is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This work is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License and the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License (COPYING) and the GNU General Public License (COPYING.GPL3).
+   If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
 #include <stdint.h>
